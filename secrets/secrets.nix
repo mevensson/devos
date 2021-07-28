93999235d0ed880c4f9c72d4a7659dbf6f06a061
Mattias Evensson <mattias@evensson.eu>
2021-07-28 19:48:31 +0200

    Add t14g1 host SSH key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,10 @@
 let
   ryzen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCK3vUzHDlEjCsIHmbNLcOruiFOBVjN8FatW1rFP4R1";
+  t14g1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFpE56AMvbwB8Wx9NanLIaMbfoNy8sYkrelVzBsCGEA";
+
   matte = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFK7dAPVafqqETZNEPwtzYVjzrR3pdEqhLSkwAF0J0rH";
-  allKeys = [ ryzen matte ];
+
+  allKeys = [ ryzen t14g1 matte ];
 in
 {
   "matte_password.age".publicKeys = allKeys;
