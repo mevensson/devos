7aecd213b3389a905c874e9ede89f0f9f2f5b4fa
Mattias Evensson <matte@evensson.eu>
2021-07-28 09:54:06 +0000

    Add t14g1 host

---
+++ hosts/t14g1/hardware.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/7f5f422d-517d-408a-b536-2b92472bcf60";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/CE9E-3512";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    {
+      device = "/dev/disk/by-uuid/7f5f422d-517d-408a-b536-2b92472bcf60";
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
+    };
+
+  swapDevices = [ ];
+
+}
