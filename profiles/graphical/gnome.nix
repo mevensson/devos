c7c54c7a25d67483d5bf0fc3671459e591643bf5
Mattias Evensson <mattias@evensson.eu>
2022-05-28 12:13:56 +0200

    Add gnome-terminal try 2

--- profiles/graphical/gnome.nix
+++ profiles/graphical/gnome.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    gnome.gnome-terminal
     gnome.gnome-tweaks
     #gnomeExtensions.arcmenu
     gnomeExtensions.appindicator
@@ -32,25 +31,30 @@
     (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
   ];
 
-  services = {
-    # Enable the X11 windowing system.
-    xserver = {
-      enable = true;
-      layout = "se";
+  programs = {
+    gnome-disks.enable;
+    gnome-terminal.enable;
+    };
 
-      # Enable touchpad support.
-      libinput = {
+    services = {
+      # Enable the X11 windowing system.
+      xserver = {
         enable = true;
-      };
+        layout = "se";
 
-      # Enable the GNOME Desktop Environment.
-      displayManager.gdm = {
-        enable = true;
-        wayland = true;
-      };
-      desktopManager.gnome = {
-        enable = true;
+        # Enable touchpad support.
+        libinput = {
+          enable = true;
+        };
+
+        # Enable the GNOME Desktop Environment.
+        displayManager.gdm = {
+          enable = true;
+          wayland = true;
+        };
+        desktopManager.gnome = {
+          enable = true;
+        };
       };
     };
-  };
-}
+  }
